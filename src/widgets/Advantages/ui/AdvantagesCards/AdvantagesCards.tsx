0d776ccfd4df9b613937cf90d10628733264ebe0
Oleg Kuzmin <info@oleg-kuzmin.ru>
2025-04-12 19:48:04 +0300

    refactor: add optimized images

--- src/widgets/Advantages/ui/AdvantagesCards/AdvantagesCards.tsx
+++ src/widgets/Advantages/ui/AdvantagesCards/AdvantagesCards.tsx
@@ -12,25 +12,25 @@ export function AdvantagesCards({ className }: Readonly<AdvantagesCardsProps>) {
       <AdvantageCard
         title="Оплата за результат"
         text="Вы платите только за переданные и получение вами целевые Лиды — их контактные данные (телефон, Telegram, email)"
-        imgSrc="/leadconveer/images/advantages-smartphone-x1.png"
+        imgSrc="/leadconveer/images/advantages-smartphone-x1-optimized.png"
         imgAlt="Телефон с оплатой картой"
       />
       <AdvantageCard
         title="Качественные лиды"
         text="Все передаваемые вам Лиды — это потенциальные клиенты, которые искали ваш товар или услугу"
-        imgSrc="/leadconveer/images/advantages-men-x1.png"
+        imgSrc="/leadconveer/images/advantages-men-x1-optimized.png"
         imgAlt="Мужчина машет рукой"
       />
       <AdvantageCard
         title="Любой объём — на ваш выбор"
         text="Покупайте сколько контактов потенциальных клиентов в месяц — сколько требуется вам!"
-        imgSrc="/leadconveer/images/advantages-basket-x1.png"
+        imgSrc="/leadconveer/images/advantages-basket-x1-optimized.png"
         imgAlt="Корзина с пакетами скидок"
       />
       <AdvantageCard
         title="Комплекс технологий"
         text="После расчёта стоимости Лидов и оплаты выбранного пакета — вы получите уникальный код для своего сайта, который позволит отслеживать контакты тех, кто не оставил заявкy"
-        imgSrc="/leadconveer/images/advantages-laptop-x1.png"
+        imgSrc="/leadconveer/images/advantages-laptop-x1-optimized.png"
         imgAlt="Ноутбук"
       />
     </div>
--- src/widgets/Hero/ui/ElementImage/ElementImage.tsx
+++ src/widgets/Hero/ui/ElementImage/ElementImage.tsx
@@ -14,6 +14,7 @@ export function ElementImage({ className }: Readonly<ElementImageProps>) {
       alt="Девушка за ноутбуком"
       width={791}
       height={497}
+      priority={true}
     />
   );
 }
